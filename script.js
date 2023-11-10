0f9260b0c9dd65506437077ebb376d1402fa8d65
Melanie <melican@LAPTOP-GKKKJDUA.>
2023-11-10 17:23:46 -0500

    more CSS & html

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
-document.querySelector("body").appendChild(h2);
+// Get the link element by its ID
+var link = document.getElementById("highlightMe");
+
+// Add a click event listener
+link.addEventListener("click", function(event) {
+  // Prevent the default behavior of the link
+  event.preventDefault();
+
+  // Toggle the 'highlight' class
+  link.classList.toggle("highlight");
+});
